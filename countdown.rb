7dc8f2c779227c88779d92afcd66c80f5067c10b
Mirim An <mimicatcodes@gmail.com>
2016-08-04 05:34:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  num = 10
+  while num >0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  num = 10
+  while num >0
+    puts "#{num} SECOND(S)!"
+    sleep(1)
+    num -= 1
+
+  end
+    return "HAPPY NEW YEAR!"
 end
